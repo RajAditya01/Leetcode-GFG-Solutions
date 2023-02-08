fda91cb0ea6736b9005a4e3c956746195e41ed44
Aditya <adityavishal903@gmail.com>
2023-02-08 23:27:50 +0530

    LeetCode

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,14 +1,32 @@
+//brute force solution in c++
+
+//Method 1
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int t) {
+        // Brute force
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]+nums[j]==t){
+                    return {i,j};
+                }
+            }
+        }
+        return{-1,-1};
+    }
+        
+};
+
 //In java using hasmap
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map  = new HashMap();
-        
         //Fill HM
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],i);
         }
-            
-            
         //Searching
          for(int i=0;i<nums.length;i++){ //2 7 11 15  target = 4
             int num = nums[i]; 
@@ -19,9 +37,6 @@ class Solution {
                 return new int[]{i,index}; 
             }  
         }
-        
         return new int[]{};
-        
     }
-        
-    }
+}
