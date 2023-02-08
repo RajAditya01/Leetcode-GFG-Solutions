44513daa86f13014c99127495099cc29f968d7c9
Aditya <adityavishal903@gmail.com>
2023-02-08 22:16:06 +0530

    gfg

---
+++ Reverse an array in group.cpp
@@ -0,0 +1,31 @@
+       int i=k,c=0,p=1;
+
+        vector<long long> v;
+
+        while(i<n){
+
+            for(int j=p*k-1;j>=k*c;j--){
+
+                v.push_back(arr[j]);
+
+            }
+
+            i=i+k;
+
+            c++;p++;
+
+        }
+
+        for(int j=n-1;j>=k*c;j--){
+
+            v.push_back(arr[j]);
+
+        }
+
+        for(int j=0;j<n;j++){
+
+            arr[j]=v[j];
+
+       }
+
+    }
