e805ccf40be4874a008c400faf224c34f38ffd9c
Aditya <adityavishal903@gmail.com>
2023-02-13 00:28:25 +0530

    Codestudio

---
+++ EquilibriumIndex.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+int findEquilibriumIndex(vector<int> &arr) {
+     
+    int n = arr.size();
+    int left =0;
+    int right =0, sum = 0;
+    
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    right=sum;
+    
+    for(int i=0;i<n;i++)
+    {
+        left+=arr[i];
+        
+        if(right==left) return i;
+        
+        right-=arr[i];
+    }
+    return -1;
+    
+}
