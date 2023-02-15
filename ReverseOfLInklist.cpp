0eea690dbee1f252acf7db9670a69dbbba7c85b8
Aditya <adityavishal903@gmail.com>
2023-02-15 19:22:35 +0530

    Leetcode

---
+++ ReverseOfLInklist.cpp
@@ -0,0 +1,17 @@
+Node* reverse(Node* &head){
+
+    Node* prevptr =NULL;
+    Node* currptr =head;
+    Node* nextptr;
+
+    while(currptr!=NULL){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+    }
+
+    return prevptr;
+
+}
