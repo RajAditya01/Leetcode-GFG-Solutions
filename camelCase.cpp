89bcbc688a88bb06dba515860fd202ac8e073c71
Aditya <adityavishal903@gmail.com>
2023-03-05 11:47:51 +0530

    .

--- camelCase.cpp
+++ camelCase.cpp
@@ -1,25 +1,32 @@
-// #include<iostream>
-// #include <string>
-// #include <cctype>
-// using namespace std;
+/* 
+ Type 1
 
-// int main(){
-//     string input="i am aditya";
-//     string output="";
+#include<iostream>
+#include <string>
+#include <cctype>
+using namespace std;
 
-//     for(int i=0; i<input.size();i++){
-//         if(input[i]==' ' ||input[i]=='_'){
-//             i++;
-//             output+=toupper(input[i]);
-//         }
-//         else{
-//             output+=input[i];
-//         }
-//     }
-//     cout<< output << endl;
-//     return 0;
-// }
+int main(){
+    string input="i am aditya";
+    string output="";
 
+    for(int i=0; i<input.size();i++){
+        if(input[i]==' ' ||input[i]=='_'){
+            i++;
+            output+=toupper(input[i]);
+        }
+        else{
+            output+=input[i];
+        }
+    }
+    cout<< output << endl;
+    return 0;
+}
+*/
+
+
+/*
+Type 2
 
 #include <iostream>
 #include <string>
@@ -39,6 +46,39 @@ int main() {
         } else if (capitalizeNext) {
             camelCaseString += toupper(originalString[i]);
             capitalizeNext = false;
+        } else {
+            camelCaseString += originalString[i];
+        }
+    }
+
+    cout << camelCaseString << endl;
+
+    return 0;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string originalString = "this_is_an_example_string";
+    string camelCaseString = "";
+
+    bool capitalizeNext = true;
+
+    for (int i = 0; i < originalString.length(); i++) {
+        if (originalString[i] == ' ' || originalString[i] == '_') {
+            capitalizeNext = true;
+        } else if (capitalizeNext) {
+            camelCaseString += toupper(originalString[i]);
+            capitalizeNext = false;
+        } else if (isupper(originalString[i])) {
+            camelCaseString += " ";
+            camelCaseString += originalString[i];
         } else {
             camelCaseString += originalString[i];
         }
