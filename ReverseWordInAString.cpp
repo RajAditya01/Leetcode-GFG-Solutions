162fce79110549e275c2b5aa77402a27c0178156
Aditya <adityavishal903@gmail.com>
2023-03-14 12:46:15 +0530

    gfg

---
+++ ReverseWordInAString.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        stack<string>st;
+        int n=S.size();
+        int i=0;
+        while(i<n){
+            int j=i;
+            string str="";
+            while(j<n&&S[j]!='.'){
+                str+=S[j];
+                j++;
+            }
+            st.push(str);
+            i=j+1;
+            
+        }
+        string res="";
+        while(st.size()){
+            if(st.size()!=1){
+                res+=st.top()+'.';
+            }
+            else{
+                res+=st.top();
+            }
+            st.pop();
+        }
+        return res;
+        
+        
+            
+           
+        
+    } 
+};
